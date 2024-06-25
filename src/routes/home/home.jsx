1a32406f0d3a390e65519dddf9257772579f2b1d
burjepaul <burjepaul97@gmail.com>
2024-06-25 19:08:45 +0300

    second commit

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './home.styles.scss'
+import Button from '../../components/button/button'
+import { Link } from "react-router-dom"
 
 const Home = () => {
     return (
-      <div>
+      <div className='home-container'>
+        
         <div className='hero-container'>
-            <p>fsadfsd</p>
+
+          <div className='hero-content'>
+            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing.</h2>
+          <div className="hero-button">
+            <Link className="nav-link-contact" to='/contact'>
+              <Button buttonText="Find more" model="1"/>
+            </Link>
+          </div>
+          </div>
+
+
         </div>
       </div>
     )
